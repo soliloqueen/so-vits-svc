477beacd22a32baff4846d690962c7785fd7632a
Baked Pineapple <bakedpineapple@airmail.cc>
2023-01-24 21:06:17 -0800

    (untested) talknet integration

--- inference_gui2.py
+++ inference_gui2.py
@@ -11,14 +11,15 @@ from pathlib import Path
 import PyQt5.QtCore as QtCore
 from PyQt5.QtGui import (QIntValidator)
 from PyQt5.QtWidgets import (QApplication, QMainWindow,
-                               QFrame, QFileDialog, QLineEdit,
-                               QPushButton, QVBoxLayout, QLabel,
-                               QComboBox, QGroupBox)
+   QFrame, QFileDialog, QLineEdit,
+   QPushButton, QHBoxLayout, QVBoxLayout, QLabel,
+   QPlainTextEdit, QComboBox, QGroupBox, QCheckBox)
 import numpy as np
 import soundfile
 import glob
 import json
 import torch
+import datetime
 from collections import deque
 from pathlib import Path
 
@@ -26,12 +27,15 @@ from inference import infer_tool
 from inference import slicer
 from inference.infer_tool import Svc
 
-#PSOLA_AVAILABLE = False
-#if importlib.util.find_spec('psola'):
-    #PSOLA_AVAILABLE = True
-    #import psola
 import librosa
 
+if importlib.util.find_spec("requests"):
+    import requests
+    REQUESTS_AVAILABLE = True
+else:
+    REQUESTS_AVAILABLE = False
+
+TALKNET_ADDR = "127.0.0.1:8050"
 MODELS_DIR = "models"
 JSON_NAME = "inference_gui2_persist.json"
 def get_speakers():
@@ -124,47 +128,47 @@ class InferenceGui2 (QMainWindow):
         self.cached_file_dir = os.path.abspath(".")
         self.recent_dirs = deque(maxlen=10)
 
-        self.sovits_frame = QGroupBox(self)
-        self.sovits_frame.setTitle("so-vits-svc")
-        self.sovits_lay = QVBoxLayout(self.sovits_frame)
-
-        self.load_persist()
-
-        # Cull non-existent paths from recent_dirs
-        self.recent_dirs = deque(
-            [d for d in self.recent_dirs if os.path.exists(d)])
-
         self.svc_model = []
 
         self.setWindowTitle("so-vits-svc GUI")
         self.central_widget = QFrame()
-        self.layout = QVBoxLayout(self.central_widget)
+        self.layout = QHBoxLayout(self.central_widget)
         self.setCentralWidget(self.central_widget)
-        #self.layout.addWidget(self.sovits_frame)
-        
-        self.header = QLabel("sof-vits")
-        self.layout.addWidget(self.header)
 
+        self.sovits_frame = QGroupBox(self)
+        self.sovits_frame.setTitle("so-vits-svc")
+        self.sovits_frame.setStyleSheet("padding:10px")
+        self.sovits_lay = QVBoxLayout(self.sovits_frame)
+        self.layout.addWidget(self.sovits_frame)
+
+        self.load_persist()
+        if self.try_connect_talknet():
+            self.try_load_talknet()
+
+        # Cull non-existent paths from recent_dirs
+        self.recent_dirs = deque(
+            [d for d in self.recent_dirs if os.path.exists(d)])
+        
         self.speaker_box = QComboBox()
         for spk in self.speakers:
             self.speaker_box.addItem(spk["name"])
         self.speaker_label = QLabel("Speaker:")
-        self.layout.addWidget(self.speaker_label)
-        self.layout.addWidget(self.speaker_box)
+        self.sovits_lay.addWidget(self.speaker_label)
+        self.sovits_lay.addWidget(self.speaker_box)
         self.speaker_box.currentIndexChanged.connect(self.try_load_speaker)
         self.try_load_speaker(0)
 
         self.file_button = FileButton()
-        self.layout.addWidget(self.file_button)
+        self.sovits_lay.addWidget(self.file_button)
         self.file_label = QLabel("Files: "+str(self.clean_files))
-        self.layout.addWidget(self.file_label)
+        self.sovits_lay.addWidget(self.file_label)
         self.file_button.clicked.connect(self.file_dialog)
         self.file_button.fileDropped.connect(self.update_files)
 
         self.recent_label = QLabel("Recent Directories:")
-        self.layout.addWidget(self.recent_label)
+        self.sovits_lay.addWidget(self.recent_label)
         self.recent_combo = QComboBox()
-        self.layout.addWidget(self.recent_combo)
+        self.sovits_lay.addWidget(self.recent_combo)
         self.update_recent_combo()
         self.recent_combo.activated.connect(self.recent_dir_dialog)
 
@@ -176,23 +180,23 @@ class InferenceGui2 (QMainWindow):
         self.source_transpose_num = QLineEdit('0')
         self.source_transpose_num.setValidator(self.transpose_validator)
         #if PSOLA_AVAILABLE:
-        self.layout.addWidget(self.source_transpose_label)
-        self.layout.addWidget(self.source_transpose_num)
+        self.sovits_lay.addWidget(self.source_transpose_label)
+        self.sovits_lay.addWidget(self.source_transpose_num)
 
         self.transpose_label = QLabel("Transpose")
-        self.layout.addWidget(self.transpose_label)
+        self.sovits_lay.addWidget(self.transpose_label)
         self.transpose_num = QLineEdit('0')
-        self.layout.addWidget(self.transpose_num)
+        self.sovits_lay.addWidget(self.transpose_num)
         self.transpose_num.setValidator(self.transpose_validator)
 
-        self.output_button = QPushButton("Output Directory")
-        self.layout.addWidget(self.output_button)
+        self.output_button = QPushButton("Change Output Directory")
+        self.sovits_lay.addWidget(self.output_button)
         self.output_label = QLabel("Output directory: "+str(self.output_dir))
-        self.layout.addWidget(self.output_label)
+        self.sovits_lay.addWidget(self.output_label)
         self.output_button.clicked.connect(self.output_dialog)
 
         self.convert_button = QPushButton("Convert")
-        self.layout.addWidget(self.convert_button)
+        self.sovits_lay.addWidget(self.convert_button)
         self.convert_button.clicked.connect(self.convert)
 
     def update_files(self, files):
@@ -205,14 +209,138 @@ class InferenceGui2 (QMainWindow):
             self.recent_dirs.append(dir_path)
         self.update_recent_combo()
 
+    # Tests for TalkNet connection and compatibility
+    def try_connect_talknet(self):
+        import socket
+        if not REQUESTS_AVAILABLE:
+            print("requests library unavailable; not loading talknet options")
+            return False
+        if not self.talknet_addr:
+            self.talknet_addr = TALKNET_ADDR
+        spl = self.talknet_addr.split(':')
+        if (spl is None) or (len(spl) == 1):
+            print("Couldn't parse talknet address "+self.talknet_addr)
+            return False
+        ip = spl[0]
+        port = int(spl[1])
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        try:
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                print("Successfully found TalkNet on address "+self.talknet_addr)
+                sock.close()
+                return True
+            else:
+                print("Could not find TalkNet on address "+self.talknet_addr)
+                sock.close()
+                return False
+        except socket.gaierror:
+            print("Couldn't connect to talknet address "+self.talknet_addr)
+            sock.close()
+            return False
+        except socket.error:
+            print("Couldn't connect to talknet address "+self.talknet_addr)
+            sock.close()
+            return False
+        sock.close()
+        return False
+
+    def try_load_talknet(self):
+        self.talknet_frame = QGroupBox(self)
+        self.talknet_frame.setTitle("talknet")
+        self.talknet_frame.setStyleSheet("padding:10px")
+        self.talknet_lay = QVBoxLayout(self.talknet_frame)
+        self.layout.addWidget(self.talknet_frame)
+
+        self.character_box = QComboBox()
+        self.character_label = QLabel("Speaker:")
+        response = requests.get('http://'+self.talknet_addr+'/characters')
+        if response.status_code == 200:
+            self.talknet_chars = json.loads(response.text)
+
+        for k in self.talknet_chars.keys():
+            self.character_box.addItem(k)
+        self.talknet_lay.addWidget(self.character_label)
+        self.talknet_lay.addWidget(self.character_box)
+        self.character_box.currentTextChanged.connect(self.talknet_character_load)
+        if len(self.talknet_chars.keys()):
+            self.cur_talknet_char = self.talknet_chars.keys()[0]
+        else:
+            self.cur_talknet_char = "N/A"
+
+        self.talknet_file_button = QPushButton("Provide input audio")
+        self.talknet_file = ""
+        self.talknet_file_label = QLabel("File: "+self.talknet_file)
+        self.talknet_lay.addWidget(self.talknet_file_button)
+        self.talknet_lay.addWidget(self.talknet_file_label)
+        self.talknet_file_button.clicked.connect(self.talknet_file_dialog)
+
+        self.talknet_transpose_label = QLabel("Transpose")
+        self.talknet_transpose_num = QLineEdit('0')
+        self.talknet_transpose_num.setValidator(self.transpose_validator)
+        self.talknet_lay.addWidget(self.talknet_transpose_label)
+        self.talknet_lay.addWidget(self.talknet_transpose_num)
+        # .text()
+
+        self.talknet_transcript_label = QLabel("Transcript")
+        self.talknet_transcript_edit = QPlainTextEdit()
+        self.talknet_lay.addWidget(self.talknet_transcript_label)
+        self.talknet_lay.addWidget(self.talknet_transcript_edit)
+        # .toPlainText()
+
+        self.talknet_sovits = QCheckBox("Push to so-vits-svc")
+        self.talknet_lay.addWidget(self.talknet_sovits)
+        # .isChecked()
+
+        self.talknet_gen_button = QPushButton("Generate")
+        self.talknet_lay.addWidget(self.talknet_gen_button)
+
+        self.talknet_output_info = QLabel("--output info (empty)--")
+        self.talknet_output_info.setWordWrap(True)
+        self.talknet_lay.addWidget(self.talknet_gen_button)
+
+        # TODO optional transcript output?
+        # TODO should be able to specify output directory
+        # TODO fancy concurrent processing stuff
+
+    def talknet_character_load(self, k):
+        self.cur_talknet_char = self.talknet_chars[k]
+
+    def talknet_generate_request(self):
+        req_time = datetime.now().strftime("%H:%M:%S")
+        response = requests.post('http://'+self.talknet_addr+'/upload',
+            data=json.dumps({'char':self.cur_talknet_char,
+                'wav':self.talknet_file,
+                'transcript':self.talknet_transcript_edit.toPlainText(),
+                'results_dir':self.output_dir}))
+        if response.status_code != 200:
+            print ("TalkNet generate request failed")
+            return
+        res = json.loads(response.text)
+        if self.talknet_sovits.isChecked():
+            res_path = self.convert([res["output_path"]])
+        self.talknet_output_info.setText("Last successful request: "+req_time+'\n'+
+            "ARPAbet: "+res.get("arpabet","N/A")+'\n'+
+            "Output path: "+res.get("output_path","N/A")+'\n')
+
+
     def try_load_speaker(self, index):
         self.speaker = self.speakers[index]
         print ("Loading "+self.speakers[index]["name"])
         self.svc_model = Svc(self.speakers[index]["model_path"],
             self.speakers[index]["cfg_path"])
 
+    def talknet_file_dialog(self):
+        files = (QFileDialog.getOpenFileName(self, "File to process",
+            options=(QFileDialog.ReadOnly | QFileDialog.ExistingFile))[0])
+        if (files is None) or (len(files) == 0):
+            return
+        self.talknet_file = files[0]
+        self.talknet_file_label.setText("File: "+str(self.talknet_file))
+
     def file_dialog(self):
-        print("opening file dialog")
+        # print("opening file dialog")
         if not self.recent_dirs:
             self.update_files(QFileDialog.getOpenFileNames(
                 self, "Files to process")[0])
@@ -221,7 +349,7 @@ class InferenceGui2 (QMainWindow):
                 self, "Files to process", self.recent_dirs[-1])[0])
 
     def recent_dir_dialog(self, index):
-        print("opening dir dialog")
+        # print("opening dir dialog")
         if not os.path.exists(self.recent_dirs[index]):
             print("Path did not exist: ", self.recent_dirs[index])
         self.update_files(QFileDialog.getOpenFileNames(
@@ -252,17 +380,18 @@ class InferenceGui2 (QMainWindow):
             o = json.load(f)
             self.recent_dirs = deque(o.get("recent_dirs",[]))
             self.output_dir = o.get("output_dir",os.path.abspath("./results/"))
+            self.talknet_addr = o.get("talknet_addr", TALKNET_ADDR)
 
     def push_pitch(self):
         pass
         # TODO
 
-    def convert(self):
+    def convert(self, clean_files = []):
         try:
             source_trans = int(self.source_transpose_num.text())
             dry_trans = int(self.transpose_num.text())
             trans = int(self.transpose_num.text()) - source_trans
-            for clean_name in self.clean_files:
+            for clean_name in clean_files:
                 infer_tool.format_wav(clean_name)
                 wav_path = Path(clean_name).with_suffix('.wav')
                 wav_name = Path(clean_name).stem
@@ -296,6 +425,7 @@ class InferenceGui2 (QMainWindow):
                 res_path = os.path.join(self.output_dir,
                     f'{wav_name}_{source_trans}_{dry_trans}key_{self.speaker["name"]}.{wav_format}')
                 soundfile.write(res_path, audio, self.svc_model.target_sample, format=wav_format)
+                return res_path
         except Exception as e:
             traceback.print_exc()
 
